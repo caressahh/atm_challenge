6ac257f1ae6177ca4c8fbed1517c7b17b947890f
Carissa <carissa@edgren.nu>
2017-06-12 22:54:15 +0200

    added expiration check of card

--- lib/atm.rb
+++ lib/atm.rb
@@ -6,17 +6,27 @@ class Atm
     @funds = 1000
   end
 
-  def withdraw(amount, account)
+  def withdraw(amount, pin_code, account)
     case
-    when insufficient_funds_in_account?(amount, account)
+  when insufficient_funds_in_account?(amount, account)
       { status: false, message: 'insufficient funds', date:Date.today }
-when insufficient_funds_in_atm?(amount)
+  when insufficient_funds_in_atm?(amount)
   { status: false, message: 'insufficient funds in ATM', date: Date.today }
+when incorrect_pin?(pin_code, account.pin_code)
+  { status: false, message: 'wrong pin', date: Date.today }
+when card_expired?(account.exp_date)
+  { status: false, message: 'card expired', date: Date.today }
   else
     perform_transaction(amount, account)
   end
 end
 private
+def card_expired?(exp_date)
+  Date.strptime(exp_date, '%m/%y') < Date.today
+end
+def incorrect_pin?(pin_code, actual_pin)
+ pin_code != actual_pin
+end
 def insufficient_funds_in_account?(amount, account)
   amount > account.balance
 end

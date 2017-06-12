b5670e5d104550a06198b048fd39fab76f198e99
Carissa <carissa@edgren.nu>
2017-06-12 22:39:38 +0200

    added insufficient funds methods

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,30 @@
 require './lib/atm.rb'
 describe Atm do
-  it 'has 1000$ on initialize' do
+  let(:account) {instance_double('Account')}
+
+  before do
+    allow(account).to receive(:balance).and_return(100)
+    allow(account).to receive(:balance=)
+  end
+  it 'has $1000 on initialize' do
     expect(subject.funds).to eq 1000
+  end
+
+  it 'funds are reduced to 950 with withdraw of 50' do
+    subject.withdraw(50, account)
+    expect(subject.funds).to eq 950
+  end
+  it 'allow withdraw if account has enough balance.' do
+    expected_output = {status: true, message: 'success', date: Date.today, amount: 45 }
+    expect(subject.withdraw(45, account)).to eq expected_output
+  end
+  it 'rejects withdraw if account has insufficient funds' do
+    expected_output = {status: false, message: 'insufficient funds', date: Date.today }
+    expect( subject.withdraw(105, account)).to eq expected_output
+  end
+  it 'rejects withdraw if ATM has insufficient funds' do
+    subject.funds = 50
+    expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
+    expect(subject.withdraw(100,account)).to eq expected_output
   end
 end

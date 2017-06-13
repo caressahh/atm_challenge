b49da501c492fc78a99ca0287164b338fd40c15c
Carissa <carissa@edgren.nu>
2017-06-13 15:22:46 +0200

    added deactivate instance method

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,7 +4,19 @@ describe Account do
   it 'is expected to have a 4 digit pin number on initialize' do
     pin_length = Math.log10(subject.pin_code).to_i + 1
     expect(pin_length).to eq 4
-
   end
+
+  it ' is expected to have a balance of 0 on initalize' do
+    expect(subject.balance).to eq 0
+  end
+
+  it 'is expected to have :active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+
+  it 'deactivates account using Instance method' do
+  subject.deactivate
+  expect(subject.account_status).to eq :deactivated
+end
 
 end
